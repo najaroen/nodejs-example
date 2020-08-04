344a1a119e055ca1df3febc305ef6e623e8588f5
TIKJA <najaroen_tik@live.com>
2020-08-04 14:21:04 +0700

    add path

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const { authUser } = require('./middleware/auth')
+const { authUser, authRole } = require('./middleware/auth')
 const app = express();
 
 app.use(authUser)
-app.get('/', (req, res, next) => {
-    res.send("Hi")
+app.use(authRole)
+app.use('/add-product', (req, res, next) => {
+    res.send(`add "Product"`)
 })
 app.listen(3000, () => {
     console.log(`running at port 3000`)
